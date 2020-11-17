2dcce3140ce934ae301a9910aebb104b78fa3bc1
worgflores <worgflores@gmail.com>
2020-11-17 20:59:00 +0800

    appserver: added vehicle

---
+++ appserver/vehicle/app-modules/rameses-gov-etracs-vehicle-services.jar/vehicle/facts/VehicleApplication.groovy
@@ -0,0 +1,50 @@
+package vehicle.facts;
+
+import java.util.*;
+import java.rmi.server.*;
+
+public class VehicleApplication {
+	
+	int appyear;
+	Date appdate;
+	String apptype;
+	String vehicletype;
+	String objid;
+	String controlno;
+	int lastfranchiseyearpaid;
+	Date nextfranchiserenewaldate;
+
+	def fixDate( def dt ) {
+		if( dt instanceof String ) {
+			def df = new java.text.SimpleDateFormat( "yyyy-MM-dd");
+			dt = df.parse( dt );
+		}
+		return dt;
+	}
+
+	public VehicleApplication() {
+	}		
+
+	public VehicleApplication( def m ) {
+		controlno = m.franchise.controlno;
+		apptype = m.apptype;
+		objid = m.objid;
+		vehicletype  = m.franchise.vehicletype.objid;
+		if( m.appyear ) appyear = m.appyear;
+		if( m.year ) appyear = m.year;
+		if( m.appdate ) {
+			appdate = fixDate( m.appdate );
+		}	
+		if(m.franchise?.lastfranchiseyearpaid) {
+			lastfranchiseyearpaid = m.franchise.lastfranchiseyearpaid;
+		}
+		if(m.franchise?.nextfranchiserenewaldate) {
+			nextfranchiserenewaldate = m.franchise.nextfranchiserenewaldate;
+		}
+	}
+
+	public void setAppdate( def dt ) {
+		appdate = fixDate( dt );		
+	}
+
+}
