d241997d9ce0b30cda5d48f8803f504913e0ecd5
worgflores <worgflores@gmail.com>
2021-05-16 16:34:24 +0800

    appserver: obo: updated

--- appserver/obo/server-modules/rameses-gov-etracs-obo-services.jar/views/vw_occupancy_certificate.mssql.sql
+++ appserver/obo/server-modules/rameses-gov-etracs-obo-services.jar/views/vw_occupancy_certificate.mssql.sql
@@ -1,85 +1,73 @@
 if object_id('dbo.vw_occupancy_certificate', 'V') IS NOT NULL 
    drop view dbo.vw_occupancy_certificate; 
 go
-CREATE VIEW vw_occupancy_certificate AS select 
-a.objid AS objid,
-a.doctypeid AS doctypeid,
-a.appno AS appno,
-a.appdate AS appdate,
-a.trackingno AS trackingno,
-a.applicantid AS applicantid,
-a.contact_name AS contact_name,
-a.contact_detail AS contact_detail,
-a.contact_email AS contact_email,
-a.contact_mobileno AS contact_mobileno,
-a.contact_phoneno AS contact_phoneno,
-a.txnmode AS txnmode,
-a.orgcode AS orgcode,
-a.createdby_objid AS createdby_objid,
-a.createdby_name AS createdby_name,
-a.dtcreated AS dtcreated,
-op.apptype AS apptype,
-op.txntype AS txntype,
-op.taskid AS taskid,
-op.bldgpermitid AS bldgpermitid,
-op.totalmaterialcost AS totalmaterialcost,
-op.totaldirectlaborcost AS totaldirectlaborcost,
-op.totalequipmentcost AS totalequipmentcost,
-op.totalothercost AS totalothercost,
-op.occupancytypeid AS occupancytypeid,
-op.actualnumunits AS actualnumunits,
-op.actualtotalfloorarea AS actualtotalfloorarea,
-op.actualheight AS actualheight,
-op.actualnumfloors AS actualnumfloors,
-op.dtactualstarted AS dtactualstarted,
-op.dtactualcompleted AS dtactualcompleted,
-op.inspectiondate AS inspectiondate,
-op.supervisorid AS supervisorid,
-op.contractor_name AS contractor_name,
-op.contractor_address AS contractor_address,
-op.contractor_pcab_idno AS contractor_pcab_idno,
-op.contractor_pcab_dtvalid AS contractor_pcab_dtvalid,
-op.contractor_tin AS contractor_tin,
-op.contractor_manager_name AS contractor_manager_name,
-op.contractor_manager_email AS contractor_manager_email,
-op.actualfixedcost AS actualfixedcost,
-op.actualprojectcost AS actualprojectcost,
-op.occupancystate AS occupancystate,
-op.amount AS amount,
-t.state AS task_state,
-t.startdate AS task_startdate,
-t.enddate AS task_enddate,
-t.assignee_objid AS task_assignee_objid,
-t.assignee_name AS task_assignee_name,
-t.actor_objid AS task_actor_objid,
-t.actor_name AS task_actor_name,
-(
-	select sys_wf_node.title from sys_wf_node 
-	where sys_wf_node.processname = 'occupancy_certificate' 
-		and sys_wf_node.name = t.state
-) AS task_title, 
-ot.objid AS occupancytype_objid,
-ot.title AS occupancytype_title,
-od.objid AS occupancytype_division_objid,
-od.title AS occupancytype_division_title,
-og.objid AS occupancytype_group_objid,
-og.title AS occupancytype_group_title,
-ctl.controlno AS controlno,
-ctl.expirydate AS expirydate,
-ctl.dtissued AS dtissued,
-ctl.issuedby_name AS issuedby_name,
-ctl.approverid AS approverid,
-ctl.endorserid AS endorserid,
-ctl.template AS template,
-ctl.reportheader AS reportheader,
-bp.title AS title,
-bp.location_text AS location_text 
-from obo_app a 
-	inner join occupancy_certificate op on a.objid = op.objid  
-	inner join vw_building_permit bp on op.bldgpermitid = bp.objid  
-	inner join occupancy_certificate_task t on op.taskid = t.taskid  
-	inner join obo_occupancy_type ot on op.occupancytypeid = ot.objid  
-	inner join obo_occupancy_type_division od on ot.divisionid = od.objid  
-	inner join obo_occupancy_type_group og on od.groupid = og.objid  
-	left join obo_control ctl on (ctl.appid = a.objid and ctl.doctypeid = 'OCCUPANCY_CERTIFICATE')
+
+CREATE VIEW vw_occupancy_certificate AS 
+SELECT 
+   a.*,
+   op.apptype,
+   op.txntype,
+   op.taskid,
+   op.bldgpermitid,
+   op.totalmaterialcost,
+   op.totaldirectlaborcost,
+   op.totalequipmentcost,
+   op.totalothercost,
+   op.occupancytypeid,
+   op.actualnumunits,
+   op.actualtotalfloorarea,
+   op.actualheight,
+   op.actualnumfloors,
+   op.dtactualstarted,
+   op.dtactualcompleted,
+   op.inspectiondate,
+   op.supervisorid,
+   op.contractor_name,
+   op.contractor_address,
+   op.contractor_pcab_idno,
+   op.contractor_pcab_dtvalid,
+   op.contractor_tin,
+   op.contractor_manager_name,
+   op.contractor_manager_email,
+   op.actualfixedcost,
+   op.actualprojectcost,
+   op.occupancystate,
+   op.amount,
+
+   t.state AS task_state,
+   t.startdate AS task_startdate,
+   t.enddate AS task_enddate,
+   t.assignee_objid AS task_assignee_objid,
+   t.assignee_name AS task_assignee_name,
+   t.actor_objid AS task_actor_objid,
+   t.actor_name AS task_actor_name,
+   (SELECT title FROM sys_wf_node WHERE processname = 'occupancy_certificate' AND name=t.state) AS task_title,
+   ot.objid AS occupancytype_objid,
+   ot.title AS occupancytype_title,   
+   od.objid AS occupancytype_division_objid,
+   od.title AS occupancytype_division_title,   
+   og.objid AS occupancytype_group_objid,
+   og.title AS occupancytype_group_title,
+   ctl.controlno,
+   ctl.expirydate,
+   ctl.dtissued,
+   ctl.issuedby_name,
+   ctl.approverid,
+   ctl.endorserid,
+   ctl.template,
+   ctl.reportheader,
+
+   bp.title,
+   bp.location_text,
+   op.actualfixedcost AS fixedcost,
+   op.actualprojectcost AS projectcost  
+
+FROM obo_app a
+INNER JOIN occupancy_certificate op ON  a.objid = op.objid 
+INNER JOIN vw_building_permit bp ON op.bldgpermitid = bp.objid
+INNER JOIN occupancy_certificate_task t ON op.taskid = t.taskid
+INNER JOIN obo_occupancy_type ot ON op.occupancytypeid = ot.objid 
+INNER JOIN obo_occupancy_type_division od ON ot.divisionid = od.objid 
+INNER JOIN obo_occupancy_type_group og ON od.groupid = og.objid 
+LEFT JOIN obo_control ctl ON ctl.appid=a.objid AND ctl.doctypeid = 'OCCUPANCY_CERTIFICATE' 
 go 
